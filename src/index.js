beae28b7349f0d071ff6e6d6759943199514c5b9
itzmeAB <abmunawer@gmail.com>
2023-01-24 23:47:33 +0500

    new

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
-
+const env = require('dotenv')
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use(express());
 
-app.get('/',(res,req)=>{
-    res.send("checking")
+//Constant
+env.config();
+
+//Middlewear
+app.use(bodyParser());
+
+
+
+app.get('/',(req,res,next)=>{
+    res.status(200).json({
+        message:"hello from server"
+    })
+})
+
+app.post('/data',(req,res,next)=>{
+    res.status(200).json({
+        message:req.body
+    })
 })
 
 
-app.listen(2000,()=>console.log("server is listening"));
+app.listen(process.env.PORT,
+    ()=>console.log(`server is running ${process.env.PORT}`));
