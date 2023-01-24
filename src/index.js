68b813f7988d1dd98c93e4ee2fdc1e2283fb588a
itzmeAB <abmunawer@gmail.com>
2023-01-24 21:13:45 +0500

    second commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.use(express());
+
+app.get('/',(res,req)=>{
+    res.send("checking")
+})
+
 
 app.listen(2000,()=>console.log("server is listening"));
